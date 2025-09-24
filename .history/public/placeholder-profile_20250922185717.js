07daf90db40e005a181735a65ff8b57f8f940940
faridfred <fdiraf77@gmail.com>
2025-09-24 11:25:43 +0700

    delete history

--- .history/public/placeholder-profile_20250922185717.js
+++
@@ -1,6 +0,0 @@
-/* This is a data URI of a simple placeholder image */
-/* You can replace this with actual profile images later */
-
-const placeholderImage = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 300 300"%3E%3Crect width="300" height="300" fill="%23e5e7eb"/%3E%3Cpath d="M150 110 C117 110 90 137 90 170 L90 190 L210 190 L210 170 C210 137 183 110 150 110 Z" fill="%23a1a1aa"/%3E%3Ccircle cx="150" cy="85" r="35" fill="%23a1a1aa"/%3E%3C/svg%3E';
-
-export default placeholderImage;
